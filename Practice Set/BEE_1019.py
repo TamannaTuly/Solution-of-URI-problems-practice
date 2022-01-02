18be6ce100da4cc7ccc5d2488cdb542fc7283e05
TamannaTuly <ttniger53@gmail.com>
2022-01-03 01:05:08 +0600

    Bee1019- Not solved

---
+++ Practice Set/BEE_1019.py
@@ -0,0 +1,16 @@
+# Read an integer value, which is the duration in seconds of a certain event in a factory, and inform
+# it expressed in hours:minutes:seconds.
+#
+# Input
+# The input file contains an integer N.
+#
+# Output
+# Print the read time in the input file (seconds) converted in hours:minutes:seconds like the following example.
+
+duration = int(input())
+
+hr = int(duration/3600)
+min = int(duration/60)
+sec = duration - (min*60)
+
+print(f'{hr}:{min}:{sec}')
