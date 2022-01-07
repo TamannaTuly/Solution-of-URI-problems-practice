c771915c89c07f8afea458bd91e76f0c9101af0a
TamannaTuly <ttniger53@gmail.com>
2022-01-07 19:20:59 +0600

    Bee1021- DONE

--- Practice Set 2/BEE_1021.py
+++ Practice Set 2/BEE_1021.py
@@ -7,126 +7,166 @@
 #
 # Output
 # Print the minimum quantity of banknotes and coins necessary to change the initial value, as the given example.
-userInput = float(input())
-realNumberINt = int(userInput)
-userInputF = userInput - float(realNumberINt)
-finalFLoat = float(userInputF)
-# finalFLoat = float("{:.2f}".format(userInputF))
-# print(type(finalFLoat))
-# print(finalFLoat)
-# # print(type(userInputF))
-# # print(userInputF)
-note100 = 100
-note50 = 50
-note20 = 20
-note10 = 10
-note5 = 5
-note2 = 2
-
-note1 = 1.00
-note050 = 0.50
-note025 = 0.25
-note010 = 0.10
-note005 = 0.05
-note001 = 0.01
-
+# userInput = float(input())
 # realNumberINt = int(userInput)
-# realNumberfloat = userInput - float(realNumberINt)
+# userInputF = userInput - float(realNumberINt)
+# finalFLoat = float(userInputF)
+# # finalFLoat = float("{:.2f}".format(userInputF))
+# # print(type(finalFLoat))
+# # print(finalFLoat)
+# # # print(type(userInputF))
+# # # print(userInputF)
+# note100 = 100
+# note50 = 50
+# note20 = 20
+# note10 = 10
+# note5 = 5
+# note2 = 2
+#
+# note1 = 1.00
+# note050 = 0.50
+# note025 = 0.25
+# note010 = 0.10
+# note005 = 0.05
+# note001 = 0.01
+#
+# # realNumberINt = int(userInput)
+# # realNumberfloat = userInput - float(realNumberINt)
+#
+#
+# if realNumberINt >= note100 :
+#     countNote100 = int(realNumberINt/ note100)
+#     realNumberINt = realNumberINt-(note100*countNote100)
+#     # print(countNote1)
+# else:
+#     countNote100 = 0
+#     # print(realNumber)
+# if realNumberINt >= note50:
+#     countNote50 = int(realNumberINt/ note50)
+#     realNumberINt = realNumberINt-(note50*countNote50)
+#     # print(countNote2)
+#     # print(realNumber)
+# else:
+#     countNote50 = 0
+#
+# if realNumberINt >= note20:
+#     countNote20 = int(realNumberINt/ note20)
+#     realNumberINt = realNumberINt-(note20*countNote20)
+#     # print(countNote3)
+#     # print(realNumber)
+# else:
+#     countNote20 = 0
+#
+# if realNumberINt >= note10:
+#     countNote10 = int(realNumberINt/ note10)
+#     realNumberINt = realNumberINt-(note10*countNote10)
+#     # print(countNote4)
+#     # print(realNumber)
+# else:
+#     countNote10 = 0
+#
+# if realNumberINt >= note5:
+#     countNote5 = int(realNumberINt/ note5)
+#     realNumberINt = realNumberINt-(note5*countNote5)
+#     # print(countnote5)
+#     # print(realNumber)
+# else:
+#     countNote5 = 0
+#
+# if realNumberINt >= note2:
+#     countNote2 = int(realNumberINt/ note2)
+#     realNumberINt = realNumberINt-(note2*countNote2)
+#     # print(countnote6)
+#     # print(realNumber)
+# else:
+#     countNote2 = 0
+#
+# finalFLoat = float(realNumberINt)+finalFLoat
+#
+# if finalFLoat >= note1:
+#     countNote1 = int(finalFLoat/ note1)
+#     finalFLoat = finalFLoat-(note1*countNote1)
+#     # print(countnote7)
+#     # print(realNumber)
+# else:
+#     countNote1 = 0
+# # print(realNumberINt)
+# if finalFLoat >= note050:
+#     countNote050 = int(finalFLoat/ note050)
+#     finalFLoat = finalFLoat-(note050*countNote050)
+#     # print(countnote7)
+#     # print(realNumber)
+# else:
+#     countNote050 = 0
+#
+# if finalFLoat >= note025:
+#     countNote025 = int(finalFLoat / note025)
+#     finalFLoat = finalFLoat - (note025 * countNote025)
+# else:
+#     countNote025 = 0
+#
+# if finalFLoat >= note010:
+#     countNote010 = int(finalFLoat / note010)
+#     finalFLoat = finalFLoat - (note010 * countNote010)
+# else:
+#     countNote010 = 0
+#
+# if finalFLoat >= note005:
+#     countNote005 = int(finalFLoat / note005)
+#     finalFLoat = finalFLoat - (note005 * countNote005)
+# else:
+#     countNote005 = 0
+#
+# if finalFLoat >= note001:
+#     countNote001 = int(finalFLoat / note001)
+#     finalFLoat = finalFLoat - (note001 * countNote001)
+# else:
+#     countNote001 = 0
+#
+# print(f'NOTAS:\n{countNote100} nota(s) de R$ 100.00\n{countNote50} nota(s) de R$ 50.00\n{countNote20} nota(s) de R$ 20.00\n{countNote10} nota(s) de R$ 10.00\n{countNote5} nota(s) de R$ 5.00\n{countNote2} nota(s) de R$ 2.00\nMOEDAS:\n{countNote1} moeda(s) de R$ 1.00\n{countNote050} moeda(s) de R$ 0.50\n{countNote025} moeda(s) de R$ 0.25\n{countNote010} moeda(s) de R$ 0.10\n{countNote005} moeda(s) de R$ 0.05\n{countNote001} moeda(s) de R$ 0.01')
+#
+# # realNumberfloat = userInput - float(realNumberINt)
+#
+# # print(f'{countNote100}\n{countNote50}\n{countNote20}\n{countNote10}\n{countNote5}\n{countNote2}\n{countNote1}\n')
 
+A=float(input())
+N=A
+a=N/100
+b=N%100
+print(b)
+c=b/50
+d=b%50
+e=d/20
+f=d%20
+g=f/10
+h=f%10
+i=h/5
+j=h%5
+k=j/2
+l=j%2
 
-if realNumberINt >= note100 :
-    countNote100 = int(realNumberINt/ note100)
-    realNumberINt = realNumberINt-(note100*countNote100)
-    # print(countNote1)
-else:
-    countNote100 = 0
-    # print(realNumber)
-if realNumberINt >= note50:
-    countNote50 = int(realNumberINt/ note50)
-    realNumberINt = realNumberINt-(note50*countNote50)
-    # print(countNote2)
-    # print(realNumber)
-else:
-    countNote50 = 0
-
-if realNumberINt >= note20:
-    countNote20 = int(realNumberINt/ note20)
-    realNumberINt = realNumberINt-(note20*countNote20)
-    # print(countNote3)
-    # print(realNumber)
-else:
-    countNote20 = 0
-
-if realNumberINt >= note10:
-    countNote10 = int(realNumberINt/ note10)
-    realNumberINt = realNumberINt-(note10*countNote10)
-    # print(countNote4)
-    # print(realNumber)
-else:
-    countNote10 = 0
-
-if realNumberINt >= note5:
-    countNote5 = int(realNumberINt/ note5)
-    realNumberINt = realNumberINt-(note5*countNote5)
-    # print(countnote5)
-    # print(realNumber)
-else:
-    countNote5 = 0
-
-if realNumberINt >= note2:
-    countNote2 = int(realNumberINt/ note2)
-    realNumberINt = realNumberINt-(note2*countNote2)
-    # print(countnote6)
-    # print(realNumber)
-else:
-    countNote2 = 0
-
-finalFLoat = float(realNumberINt)+finalFLoat
-
-if finalFLoat >= note1:
-    countNote1 = int(finalFLoat/ note1)
-    finalFLoat = finalFLoat-(note1*countNote1)
-    # print(countnote7)
-    # print(realNumber)
-else:
-    countNote1 = 0
-# print(realNumberINt)
-if finalFLoat >= note050:
-    countNote050 = int(finalFLoat/ note050)
-    finalFLoat = finalFLoat-(note050*countNote050)
-    # print(countnote7)
-    # print(realNumber)
-else:
-    countNote050 = 0
-
-if finalFLoat >= note025:
-    countNote025 = int(finalFLoat / note025)
-    finalFLoat = finalFLoat - (note025 * countNote025)
-else:
-    countNote025 = 0
-
-if finalFLoat >= note010:
-    countNote010 = int(finalFLoat / note010)
-    finalFLoat = finalFLoat - (note010 * countNote010)
-else:
-    countNote010 = 0
-
-if finalFLoat >= note005:
-    countNote005 = int(finalFLoat / note005)
-    finalFLoat = finalFLoat - (note005 * countNote005)
-else:
-    countNote005 = 0
-
-if finalFLoat >= note001:
-    countNote001 = int(finalFLoat / note001)
-    finalFLoat = finalFLoat - (note001 * countNote001)
-else:
-    countNote001 = 0
-
-print(f'NOTAS:\n{countNote100} nota(s) de R$ 100.00\n{countNote50} nota(s) de R$ 50.00\n{countNote20} nota(s) de R$ 20.00\n{countNote10} nota(s) de R$ 10.00\n{countNote5} nota(s) de R$ 5.00\n{countNote2} nota(s) de R$ 2.00\nMOEDAS:\n{countNote1} moeda(s) de R$ 1.00\n{countNote050} moeda(s) de R$ 0.50\n{countNote025} moeda(s) de R$ 0.25\n{countNote010} moeda(s) de R$ 0.10\n{countNote005} moeda(s) de R$ 0.05\n{countNote001} moeda(s) de R$ 0.01')
-
-# realNumberfloat = userInput - float(realNumberINt)
-
-# print(f'{countNote100}\n{countNote50}\n{countNote20}\n{countNote10}\n{countNote5}\n{countNote2}\n{countNote1}\n')
-
-
+E=A*100
+B=(int(E))
+m=B%100
+n=m/50
+o=m%50
+p=o/25
+q=o%25
+r=q/10
+s=q%10
+t=s/5
+u=s%5
+print("NOTAS:")
+print(f"{int(a)} nota(s) de R$ 100.00")
+print(f"{int(c)} nota(s) de R$ 50.00")
+print(f"{int(e)} nota(s) de R$ 20.00")
+print(f"{int(g)} nota(s) de R$ 10.00")
+print(f"{int(i)} nota(s) de R$ 5.00")
+print(f"{int(k)} nota(s) de R$ 2.00")
+print(f"MOEDAS:")
+print(f"{int(l)} moeda(s) de R$ 1.00")
+print(f"{int(n)} moeda(s) de R$ 0.50")
+print(f"{int(p)} moeda(s) de R$ 0.25")
+print(f"{int(r)} moeda(s) de R$ 0.10")
+print(f"{int(t)} moeda(s) de R$ 0.05")
+print(f"{int(u)} moeda(s) de R$ 0.01")
